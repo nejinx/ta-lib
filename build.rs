3f1917cc1a223d10f78ae66e4d8178d82a492aae
Jasper McCoubrie <jmccoubrie@outlook.com>
2017-10-30 12:35:30 +0800

    local repository

--- build.rs
+++ build.rs
@@ -6,7 +6,8 @@ fn main() {
     println!("cargo:rustc-link-lib=static=ta_abstract_cdd");
     println!("cargo:rustc-link-lib=static=ta_func_cdd");
     println!("cargo:rustc-link-lib=static=ta_libc_cdd");
-    let dir = env::var("CARGO_MANIFEST_DIR").unwrap();
+    //let dir = env::var("CARGO_MANIFEST_DIR").unwrap();
 
-    println!("cargo:rustc-link-search={}", Path::new(&dir).join("ta-lib").join("lib").display());
+    //println!("cargo:rustc-link-search={}", Path::new(&dir).join("ta-lib").join("lib").display());
+    println!("cargo:rustc-link-search={}", Path::new("c:\\").join("ta-lib").join("c").join("lib").display());
 }
