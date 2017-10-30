6c2fd9a99e3956af7a18b6935fc2cb65842723b9
Jasper McCoubrie <jmccoubrie@outlook.com>
2017-10-30 13:33:29 +0800

    adjusting lib usage
    Have to recompile the libs to support 64-bit

--- build.rs
+++ build.rs
@@ -2,10 +2,10 @@ use std::path::Path;
 use std::env;
 
 fn main() {
-    println!("cargo:rustc-link-lib=static=ta_common_cdd");
-    println!("cargo:rustc-link-lib=static=ta_abstract_cdd");
-    println!("cargo:rustc-link-lib=static=ta_func_cdd");
-    println!("cargo:rustc-link-lib=static=ta_libc_cdd");
+    println!("cargo:rustc-link-lib=static=ta_common_cmr");
+    println!("cargo:rustc-link-lib=static=ta_abstract_cmr");
+    println!("cargo:rustc-link-lib=static=ta_func_cmr");
+    println!("cargo:rustc-link-lib=static=ta_libc_cmr");
     //let dir = env::var("CARGO_MANIFEST_DIR").unwrap();
 
     //println!("cargo:rustc-link-search={}", Path::new(&dir).join("ta-lib").join("lib").display());
