ad89659af0b7e39c300137fe97897f992e86c4cb
nejinx <jmccoubrie@outlook.com>
2017-11-06 12:41:31 +0800

    changed lib to cdr, compiled 64-bit ta-lib

--- build.rs
+++ build.rs
@@ -2,12 +2,11 @@ use std::path::Path;
 use std::env;
 
 fn main() {
-    println!("cargo:rustc-link-lib=static=ta_common_cmr");
-    println!("cargo:rustc-link-lib=static=ta_abstract_cmr");
-    println!("cargo:rustc-link-lib=static=ta_func_cmr");
-    println!("cargo:rustc-link-lib=static=ta_libc_cmr");
+    println!("cargo:rustc-link-lib=static=ta_common_cdr");
+    println!("cargo:rustc-link-lib=static=ta_abstract_cdr");
+    println!("cargo:rustc-link-lib=static=ta_func_cdr");
+    println!("cargo:rustc-link-lib=static=ta_libc_cdr");
     //let dir = env::var("CARGO_MANIFEST_DIR").unwrap();
-
     //println!("cargo:rustc-link-search={}", Path::new(&dir).join("ta-lib").join("lib").display());
     println!("cargo:rustc-link-search={}", Path::new("c:\\").join("ta-lib").join("c").join("lib").display());
 }
